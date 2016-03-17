8a0d8321fbe491639d01e9b96c5d01e7f50bc8bc
jsilva <jsilva@dcom276559.legislaturachaco.local>
2016-03-17 11:17:38 -0300

    Diseño 1

--- src/fichatecnica/FichaTecnica.java
+++ src/fichatecnica/FichaTecnica.java
@@ -5,6 +5,8 @@
  */
 package fichatecnica;
 
+import ventanas.FichaTecnicaImpresion;
+
 /**
  *
  * @author coperalta
@@ -15,7 +17,8 @@ public class FichaTecnica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FichaTecnicaImpresion ft=new FichaTecnicaImpresion();
+        ft.setVisible(true);
     }
     
 }
